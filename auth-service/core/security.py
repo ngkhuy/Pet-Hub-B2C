4f481fe0bae4291b8da3509a4b03faa3deeddd89
K.Huy <ngkhuy.159@gmail.com>
2025-11-12 16:32:35 +0700

    Update username param

--- auth-service/core/security.py
+++ auth-service/core/security.py
@@ -7,11 +7,10 @@ from datetime import datetime, timedelta, timezone
 
 from core.config import settings
 
-# Config 
+# Config
 # Schema cho access token (login)
 oauth2_schema = OAuth2PasswordBearer(
-    tokenUrl="/api/auth/login",
-    scheme_name="Access Token Auth"
+    tokenUrl="/api/auth/login", scheme_name="Access Token Auth"
 )
 
 # Schema cho reset token (verify OTP)
@@ -19,14 +18,17 @@ reset_oauth2_schema = HTTPBearer(scheme_name="Reset Token Auth")
 
 pwd_context = PasswordHash.recommended()
 
+
 # Password methods
 def get_password_hash(password: str):
     """Băm mật khẩu"""
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password: str, hashed_password: str):
     """Kiểm tra mật khẩu"""
     return pwd_context.verify(plain_password, hashed_password)
+
 
 # JWT methods
 def create_jwt_token(data: dict, expires_delta: timedelta | None = None):
@@ -36,12 +38,13 @@ def create_jwt_token(data: dict, expires_delta: timedelta | None = None):
         expire = datetime.now(timezone.utc) + expires_delta
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    
+
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM)
-    
+
     return encoded_jwt
-    
+
+
 def decode_token(token: str):
     """
     Giải mã Token, kiểm tra type, và trả về payload
@@ -50,22 +53,24 @@ def decode_token(token: str):
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, [settings.JWT_ALGORITHM])
         if payload is None:
             return None
-        
+
         return payload
-    
+
     except JWTError:
         # Token hết hạn, sai chữ ký, hoặc không hợp lệ
         return None
+
 
 # OTP methods
 def generate_otp(length: int = 6):
     """Hàm tạo OTP"""
     return "".join([str(random.randint(0, 9)) for _ in range(length)])
 
+
 # Hàm giả lập sms gửi qua console thay vì sđt thật
-async def simulate_sms(phone_number: str, otp: str):
+async def simulate_sms(email: str, otp: str):
     print("------------------------------------------------")
-    print(f"MOCK SMS: Gửi OTP {otp} đến số điện thoại {phone_number}")
+    print(f"MOCK SMS: Gửi OTP {otp} đến {email}")
     print("------------------------------------------------")
-    
+
     await asyncio.sleep(0)
