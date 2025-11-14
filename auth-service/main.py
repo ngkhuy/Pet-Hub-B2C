4b5c7be8434f0b6d92010081b65447f6cc0a786e
K.Huy <ngkhuy.159@gmail.com>
2025-11-14 16:38:27 +0700

    Delete notes

--- auth-service/main.py
+++ auth-service/main.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware # <-- 1. Import
+from fastapi.middleware.cors import CORSMiddleware
 from routers import auth as auth_router
     
 app = FastAPI(title="Auth service API", 
               description="API cho dịch vụ xác thực người dùng",
               version="0.1.0")
 
-# --- 2. THÊM CẤU HÌNH CORS (BẮT BUỘC) ---
 app.add_middleware(
     CORSMiddleware,
-    # Thay đổi "http://localhost:3000" bằng địa chỉ FE của bạn
     allow_origins=["http://localhost:3000"], 
     
-    # BẮT BUỘC BẰNG TRUE ĐỂ CHO PHÉP GỬI COOKIE
     allow_credentials=True, 
     
     allow_methods=["*"], 
@@ -22,7 +19,7 @@ app.add_middleware(
 
 app.include_router(
     auth_router.router, 
-    prefix="/api/auth", # <-- Prefix này RẤT ĐÚNG
+    prefix="/api/auth",
     tags=["Auth endpoints"]
 )
 
