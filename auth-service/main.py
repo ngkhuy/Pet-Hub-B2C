86b554f40ffb36f5bd9e48ba0ec0a23843e8df5e
cuongnguyen <cuongnguyen112354@gmail.com>
2025-11-23 15:26:40 +0700

    commit

--- auth-service/main.py
+++ auth-service/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Auth service API",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["http://localhost:3000"],
     
     allow_credentials=True, 
     
